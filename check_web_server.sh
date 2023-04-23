b9c9d724edab2fcefe7daf544f35b150afc3fd4b
bxnvld <99895573+bxnvld@users.noreply.github.com>
2023-04-23 20:43:26 +0300

    Update check_web_server.sh

--- check_web_server.sh
+++ check_web_server.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# URL веб-сервера, який будемо перевіряти
 url="http://example.com"
-# Лог-файл, куди буде записано повідомлення
 log_file="error.log"
-# Адреса електронної пошти для відправки повідомлення
 email="example@example.com"
 
-# Функція для перевірки коду відповіді
 check_response_code() {
   response_code=$(curl -s -o /dev/null -w "%{http_code}" "$url")
   if [[ "$response_code" =~ ^(2|3)[0-9]{2}$ ]]; then
@@ -15,13 +11,10 @@ check_response_code() {
   else
     echo "Response code: $response_code - ERROR"
     echo "$(date) - Response code: $response_code" >> "$log_file"
-    # Для відправки повідомлення на пошту можна використати, наприклад, утиліту "mail" на Linux
-    # echo "Subject: Error in web server response code" | cat - "$log_file" | sendmail "$email"
   fi
 }
 
 while true; do
   check_response_code
-  # Задержка на 30 секунд
   sleep 30
 done
